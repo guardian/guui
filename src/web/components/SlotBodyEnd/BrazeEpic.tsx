c2ef87b2fedda2dc1b81347f06edf67d78aec8ab
Tom Wey <tom.wey@theguardian.com>
2021-07-16 16:41:06 +0100

    Decouple BrazeEpic from the RRCP component
    This moves the BrazeEpic away from "borrowing" the ContributionsEpic
    component to instead use components from @guardian/braze-components,
    just like the banner. This is required because we want to start using
    some new components to promote newsletters in this space. It was also
    fragile to rely on a component from another team/system.
    
    Note that these changes depend on some as yet unreleased changes to
    @guardian/braze-components where we've split the top level factory
    component BrazeMessageComponent into two - BrazeBannerComponent and
    BrazeEndOfArticle component.
    
    Still TODO: pass through a real implementation of subscribeToNewsletter
    and also countryCode to the component from braze-components. We should
    add countryCode to the common end-of-article component props in that
    project. Also add ophanComponentId as a required prop in braze-
    components for all end-of-article components and their canRender
    functions.

--- src/web/components/SlotBodyEnd/BrazeEpic.tsx
+++ src/web/components/SlotBodyEnd/BrazeEpic.tsx
@@ -1,58 +1,29 @@
 import { useEffect, useRef, useState } from 'react';
 import { css } from '@emotion/react';
 
-import { either } from '@guardian/types';
 import type { BrazeMessagesInterface } from '@guardian/braze-components/logic';
 
-import {
-	parseBrazeEpicParams,
-	EpicDataFromBraze,
-	Variant,
-} from '@root/src/web/lib/braze/parseBrazeEpicParams';
+import { EpicDataFromBraze } from '@root/src/web/lib/braze/parseBrazeEpicParams';
 import { getBrazeMetaFromUrlFragment } from '@root/src/web/lib/braze/forceBrazeMessage';
 import { CanShowResult } from '@root/src/web/lib/messagePicker';
 import { useOnce } from '@root/src/web/lib/useOnce';
-import { joinUrl } from '@root/src/lib/joinUrl';
 import { useHasBeenSeen } from '@root/src/web/lib/useHasBeenSeen';
 import { submitComponentEvent } from '@root/src/web/browser/ophan/ophan';
-import { setAutomat } from '@root/src/web/lib/setAutomat';
-import { MODULES_VERSION } from '@root/src/web/lib/contributions';
+import type { CommonEndOfArticleComponentProps } from '@guardian/braze-components';
 
 const wrapperMargins = css`
 	margin: 18px 0;
 `;
 
-const EPIC_COMPONENT_PATH = `/modules/${MODULES_VERSION}/epics/ContributionsEpic.js`;
 const COMPONENT_TYPE = 'RETENTION_EPIC';
 
 type Meta = {
 	dataFromBraze?: EpicDataFromBraze;
 	logImpressionWithBraze: () => void;
-	// logButtonClickWithBraze: (id: number) => void; // TODO: Handle button click tracking
 };
 
 type EpicConfig = {
 	meta: Meta;
-	contributionsServiceUrl: string;
-	countryCode: string;
-};
-
-type Tracking = {
-	ophanPageId: string;
-	platformId: string;
-	clientName: string;
-	referrerUrl: string;
-};
-
-type ArticleCounts = {
-	for52Weeks: number;
-	forTargetedWeeks: number;
-};
-
-type EpicProps = {
-	variant: Variant;
-	tracking: Tracking;
-	articleCounts: ArticleCounts;
 	countryCode: string;
 };
 
@@ -76,7 +47,7 @@ export const canShow = async (
 			meta: {
 				dataFromBraze: message.extras,
 				logImpressionWithBraze: () => {
-					message.logImpression();
+					// message.logImpression();
 				},
 			},
 		};
@@ -85,57 +56,18 @@ export const canShow = async (
 	}
 };
 
-const buildEpicProps = (
-	dataFromBraze: EpicDataFromBraze,
-	countryCode: string,
-): EpicProps | null => {
-	const variantResult = parseBrazeEpicParams(dataFromBraze);
-
-	return either<string, Variant, EpicProps | null>(
-		(err) => {
-			const msg = `Not rendering Braze epic - there are props missing: ${err}`;
-			window.guardian.modules.sentry.reportError(
-				new Error(msg),
-				'braze-epic',
-			);
-			return null;
-		},
-		(variant) => {
-			const tracking = {
-				ophanPageId: window.guardian.config.ophan.pageViewId,
-				platformId: 'GUARDIAN_WEB',
-				clientName: 'dcr',
-				referrerUrl: window.location.origin + window.location.pathname,
-
-				// We need to pass these props in order to pass the
-				// ContributionsEpic validation. This doesn't feel
-				// great, so I think a better solution will be to create our own
-				// epic component, with the same look and feel, in
-				// @guardian/braze-components which doesn't have the same prop
-				// requirements.
-				abTestName: '',
-				abTestVariant: '',
-				campaignCode: '',
-				componentType: 'ACQUISITIONS_EPIC',
-			};
-
-			const articleCounts = {
-				for52Weeks: 0,
-				forTargetedWeeks: 0,
-			};
-
-			return { variant, tracking, articleCounts, countryCode };
-		},
-	)(variantResult);
+type InnerProps = {
+	meta: Meta;
+	countryCode: string;
+	BrazeComponent: React.FC<CommonEndOfArticleComponentProps>;
 };
 
-const BrazeEpic = ({
-	contributionsServiceUrl,
+const BrazeEpicWithSatisfiedDependencies = ({
+	BrazeComponent,
 	meta,
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	countryCode,
-}: EpicConfig) => {
-	const [Epic, setEpic] = useState<React.FC<EpicProps>>();
-
+}: InnerProps) => {
 	const [hasBeenSeen, setNode] = useHasBeenSeen({
 		rootMargin: '-18px',
 		threshold: 0,
@@ -145,27 +77,10 @@ const BrazeEpic = ({
 	const epicRef = useRef(null);
 
 	useOnce(() => {
-		setAutomat();
-
-		const componentUrl = joinUrl([
-			contributionsServiceUrl,
-			EPIC_COMPONENT_PATH,
-		]);
-
-		window
-			.guardianPolyfilledImport(componentUrl)
-			.then((epicModule: { ContributionsEpic: React.FC<EpicProps> }) => {
-				setEpic(() => epicModule.ContributionsEpic); // useState requires functions to be wrapped
-			})
-			.catch((error) => {
-				window.guardian.modules.sentry.reportError(error, 'braze-epic');
-			});
-	}, [contributionsServiceUrl, meta]);
-
-	useOnce(() => {
 		submitComponentEvent({
 			component: {
 				componentType: COMPONENT_TYPE,
+				// TODO: make ophanComponentId required in the canRender of components
 				id: (meta.dataFromBraze as EpicDataFromBraze).ophanComponentId,
 			},
 			action: 'INSERT',
@@ -180,6 +95,7 @@ const BrazeEpic = ({
 			submitComponentEvent({
 				component: {
 					componentType: COMPONENT_TYPE,
+					// TODO: make ophanComponentId required in the canRender of components
 					id: meta.dataFromBraze.ophanComponentId,
 				},
 				action: 'VIEW',
@@ -187,41 +103,56 @@ const BrazeEpic = ({
 		}
 	}, [hasBeenSeen, meta]);
 
-	if (Epic && meta.dataFromBraze) {
-		// This will come from Braze via the meta from canShow
-		const props = buildEpicProps(meta.dataFromBraze, countryCode);
-
-		if (props) {
-			return (
-				<div ref={setNode} css={wrapperMargins}>
-					<div ref={epicRef}>
-						{/* eslint-disable-next-line react/jsx-props-no-spreading */}
-						<Epic {...props} />
-					</div>
+	if (meta.dataFromBraze) {
+		return (
+			<div ref={setNode} css={wrapperMargins}>
+				<div ref={epicRef}>
+					{/* eslint-disable-next-line react/jsx-props-no-spreading */}
+					<BrazeComponent
+						componentName={meta.dataFromBraze.componentName}
+						brazeMessageProps={meta.dataFromBraze}
+						// TODO: Add real implementation for subscribeToNewsletter
+						subscribeToNewsletter={() => {
+							return Promise.resolve();
+						}}
+						// countryCode={countryCode} // TODO: add countryCode to common props in braze-components
+					/>
 				</div>
-			);
-		}
+			</div>
+		);
 	}
 
 	return null;
 };
 
-export const MaybeBrazeEpic = ({
-	contributionsServiceUrl,
-	meta,
-	countryCode,
-}: EpicConfig) => {
-	const componentName = meta.dataFromBraze?.componentName;
+export const MaybeBrazeEpic = ({ meta, countryCode }: EpicConfig) => {
+	const [BrazeComponent, setBrazeComponent] = useState<
+		React.FC<CommonEndOfArticleComponentProps>
+	>();
 
-	if (componentName === 'Epic') {
-		return (
-			<BrazeEpic
-				contributionsServiceUrl={contributionsServiceUrl}
-				meta={meta}
-				countryCode={countryCode}
-			/>
-		);
-	}
+	useEffect(() => {
+		import(
+			/* webpackChunkName: "guardian-braze-components" */ '@guardian/braze-components'
+		)
+			.then((module) => {
+				setBrazeComponent(() => module.BrazeEndOfArticleComponent);
+			})
+			.catch((error) =>
+				window.guardian.modules.sentry.reportError(error, 'braze-epic'),
+			);
+	}, []);
 
-	return null;
+	return (
+		<>
+			{BrazeComponent ? (
+				<BrazeEpicWithSatisfiedDependencies
+					BrazeComponent={BrazeComponent}
+					meta={meta}
+					countryCode={countryCode}
+				/>
+			) : (
+				<div />
+			)}
+		</>
+	);
 };
